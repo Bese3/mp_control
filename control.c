b626f5cc6d6fe93093f831fa365b0598419a88d9
besufikad <07yilmabese@gmail.com>
2023-01-20 19:45:00 +0300

    mp_control

--- control.c
+++ control.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"shell.h"
 
 
 /* This project consist of the demonstration of MicroProgrammed
@@ -14,23 +15,6 @@
 
 
 
-void horizontal();
-void vertical();
-
-struct c_memory
-{
-    int memory[32];
-    int *address[32];
-};
-struct c_memory alu();
-int sum(int x , int y);
-int sub(int x , int y);
-int mul(int x , int y);
-int divide(int x , int y);
-int mod(int x , int y);
-int or(int x , int y);
-int not(int x );
-int and(int x , int y);
 
 
 
--- shell.h
+++ shell.h
@@ -1,1 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
 
+void horizontal();
+void vertical();
+
+struct c_memory
+{
+    int memory[32];
+    int *address[32];
+};
+struct c_memory alu();
+int sum(int x , int y);
+int sub(int x , int y);
+int mul(int x , int y);
+int divide(int x , int y);
+int mod(int x , int y);
+int or(int x , int y);
+int not(int x );
+int and(int x , int y);
+#endif /*SHELL_H*/
